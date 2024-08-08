0bb7418584ce72ed37261226f7e5ba51c563873c
Aniruddh Srivastava <63553175+Noir01@users.noreply.github.com>
2024-08-08 14:57:17 -0400

    Added 'cd' and 'exit' support very primitive

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, stdin, Write}, process::Command};
+use std::{env, io::{self, stdin, Write}, iter::Iterator, path::Path, process::Command};
 
 fn main() {
     loop {
@@ -16,14 +16,24 @@ fn main() {
         let command = parts.next().unwrap();
         let args: Vec<&str> = parts.collect();
 
-        match Command::new(command).args(&args).status() {
-            Ok(status) => {
-                if !status.success() {
-                    eprintln!("Command exited with status: {}", status);
+        match command {
+            "cd" => {
+                let new_dir = args.iter().peekable().peek().map_or("/", |x| *x); // Use iter() to convert args into an iterator
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
                 }
-            }
-            Err(e) => {
-                eprintln!("Failed to execute command: {}", e);
+            },
+            "exit" => return,
+            command => match Command::new(command).args(&args).status() {
+                Ok(status) => {
+                    if !status.success() {
+                        eprintln!("Command exited with status: {}", status);
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Failed to execute command: {}", e);
+                }
             }
         }
     }
